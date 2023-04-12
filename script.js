2eb5fc8c02b3f42926f424fced93b101591c47fe
pedreca <80847947+ArthurPedreca@users.noreply.github.com>
2023-04-12 18:08:52 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const inimigo = document.querySelectorAll('.cabeca', '.corpo')
-const boneco = document.querySelector('.inimigo')
-
-inimigo.forEach(function(elemento){
-    elemento.addEventListener('click', function(){
-        boneco.style.display = "none"
-    })
-})
